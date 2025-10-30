e2478f1b54183ac7b5ce2a916284d4a06a066a18
meghanath44 <meghanathpenchala@gmail.com>
2025-10-29 21:38:14 -0400

    Added Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home'
+import Result from './Components/Result'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    (<Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+      </Routes>
+    </Router>)
   )
 }
 
