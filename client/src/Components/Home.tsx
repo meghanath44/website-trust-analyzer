21adca336d34d91fa6dfd09c7d7d47d233781ac4
meghanath44 <meghanathpenchala@gmail.com>
2025-11-13 20:35:42 -0500

    handled onSubmit function with the provided URL

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -6,10 +6,18 @@ function Home (){
     const navigate = useNavigate();
     const [url, setUrl] = useState("");
     const [isInvalidUrl, setIsInvalidUrl] = useState(false);
-    const validateSubmit = (e:React.FormEvent) =>{
+    const onSubmit = async (e:React.FormEvent) =>{
         e.preventDefault();
         try{
             new URL(url);
+            const response = await fetch('http://127.0.0.1:5000/check_reputation',{
+                mode:'cors',
+                method:'POST',
+                headers:{'Content-Type':'application/json','Access-Control-Allow-Origin':'*'},
+                body:JSON.stringify({'url':url}),
+            });
+            const data= await response.json();
+            console.log(data);
             navigate("/result");
         }
         catch(e){
@@ -26,7 +34,7 @@ function Home (){
     return (
         <div>
             <h1>Website Genuineness Validator</h1>
-            <form onSubmit={validateSubmit}>
+            <form onSubmit={onSubmit}>
                 <input
                     type="text"
                     placeholder="Enter website URL"
