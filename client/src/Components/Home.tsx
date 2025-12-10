e7e647419cd95c5d517c0de53f8c3209c12ec383
meghanath44 <meghanathpenchala@gmail.com>
2025-12-10 16:55:25 -0500

    Added loading spinner feature while retrieving URL related data.

--- client/src/Components/Home.tsx
+++ client/src/Components/Home.tsx
@@ -6,8 +6,10 @@ function Home (){
     const navigate = useNavigate();
     const [url, setUrl] = useState("");
     const [isInvalidUrl, setIsInvalidUrl] = useState(false);
+    const [loading, setLoading] = useState(false);
     const onSubmit = async (e:React.FormEvent) =>{
         e.preventDefault();
+        setLoading(true);
         try{
             new URL(url);
             const response = await fetch('http://127.0.0.1:5000/check_reputation',{
@@ -23,6 +25,7 @@ function Home (){
         catch(e){
             setIsInvalidUrl(true);
         }
+        setLoading(false);
     }
 
     const onURLChange = (e:React.ChangeEvent<HTMLInputElement>) => {
@@ -33,17 +36,23 @@ function Home (){
 
     return (
         <div>
-            <h1>Website Genuineness Validator</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    placeholder="Enter website URL"
-                    value={url}
-                    onChange={(e)=>onURLChange(e)}>
-                </input>
-                <button type="submit">Check</button>
-            </form>
-            {isInvalidUrl && (<div>Invalid URL Entered!</div>)}
+            {!loading && (<>
+                <h1>Website Genuineness Validator</h1>
+                <form onSubmit={onSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Enter website URL"
+                        value={url}
+                        onChange={(e)=>onURLChange(e)}>
+                    </input>
+                    <button type="submit">Check</button>
+                </form>
+                {isInvalidUrl && (<div>Invalid URL Entered!</div>)}
+            </>)}
+            {loading && (<div >
+                <div className="spinner"></div>
+                <p>Analyzing website...</p>
+            </div>)}
         </div>
     );
 }
